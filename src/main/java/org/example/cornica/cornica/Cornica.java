3d1b4ad3053a8abc836685d28a9253f74459bfb6
lole <loganboganfarfignogan@gmail.com>
2025-02-11 17:58:17 -0600

    So everything is set up as a skelton

---
+++ src/main/java/org/example/cornica/cornica/Cornica.java
@@ -0,0 +1,17 @@
+package org.example.cornica.cornica;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Cornica extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
